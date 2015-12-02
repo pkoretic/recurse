f0924b1d2a8453b8d2baab2e48a30a77eb236282
Mario Kozjak <kozjakm1@gmail.com>
2015-12-02 18:35:02 +0100

    build_me.sh update
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- example/build_me.sh
+++ example/build_me.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-make clean && rm .qmake.stash && qmake . && make && ./recurse
+if [ `which qmake | wc -l` -lt 1 ]
+then
+	qmake='/usr/local/Cellar/qt5/5.5.1_2/bin/qmake'
+fi
+
+make clean && rm .qmake.stash
+$qmake . && make && ./recurse
