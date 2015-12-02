6720eb2747b62b4b4370e098c1e93f202b5cbae3
Mario Kozjak <kozjakm1@gmail.com>
2015-12-02 13:34:20 +0100

    initial http server compose function
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- example/main.cpp
+++ example/main.cpp
@@ -4,7 +4,18 @@ int main(int argc, char *argv[])
 {
     Recurse app(argc, argv);
 
-    app.listen(3000);
+    // app.listen(3000);
 
-    // HttpServer srv(2000);
+    HttpServer srv;
+
+    bool bound = srv.compose(3000);
+
+    if (!bound) {
+        qDebug() << "could not bind to port 3000";
+        return 0;
+    }
+
+    app.listen(&srv);
+
+    qDebug() << "ok";
 };
--- recurse.hpp
+++ recurse.hpp
@@ -16,25 +16,42 @@ class HttpServer : public QObject
     Q_OBJECT
 
 public:
-    HttpServer(quint64 port, QObject *parent = NULL);
+    HttpServer(QObject *parent = NULL);
     ~HttpServer();
+
+    bool compose(quint64 port, QHostAddress address = QHostAddress::Any);
 
 private:
     QTcpServer m_tcp_server;
     quint64 m_port;
+    QHostAddress m_address;
 };
 
-inline HttpServer::HttpServer(quint64 port, QObject *parent)
+inline HttpServer::HttpServer(QObject *parent)
 {
     Q_UNUSED(parent);
-
-    m_port = port;
-    qDebug() << "test" << m_port;
 };
 
 inline HttpServer::~HttpServer()
 {
 
+};
+
+inline bool HttpServer::compose(quint64 port, QHostAddress address)
+{
+    m_port = port;
+    m_address = address;
+
+    if (!m_tcp_server.listen(address, port)) {
+        qDebug() << "failed to start listening";
+        return false;
+    }
+
+    connect(&m_tcp_server, &QTcpServer::newConnection, [this] {
+        qDebug() << "connection!";
+    });
+
+    return true;
 };
 
 //!
@@ -50,9 +67,11 @@ public:
     ~Recurse();
 
     bool listen(quint64 port, QHostAddress address = QHostAddress::Any);
+    bool listen(HttpServer *server);
 
 private:
     QCoreApplication app;
+    HttpServer *srv;
 };
 
 inline Recurse::Recurse(int & argc, char ** argv, QObject *parent) : app(argc, argv)
@@ -65,12 +84,35 @@ inline Recurse::~Recurse()
 
 };
 
+//!
+//! \brief Recurse::listen
+//! listen for tcp requests
+//!
+//! \param port tcp server port
+//! \param address tcp server listening address
+//!
+//! \return true on success
+//!
 inline bool Recurse::listen(quint64 port, QHostAddress address)
 {
-    qDebug() << "listening on port" << port;
+    srv->compose(port, address);
 
-    HttpServer srv(2000);
+    return app.exec();
+};
 
+//!
+//! \brief Recurse::listen
+//! listen for tcp requests
+//!
+//! Overloaded function
+//!
+//! \param server pointer to the HttpServer instance
+//!
+//! \return true on success
+//!
+inline bool Recurse::listen(HttpServer *server)
+{
+    srv = server;
     return true;
 };
 
