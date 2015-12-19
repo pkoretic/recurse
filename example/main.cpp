d2303999d960dd55f1b5e17abbad7ba631cedf4c
Mario Kozjak <kozjakm1@gmail.com>
2015-12-19 15:01:12 +0100

    example cleanup
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- example/main.cpp
+++ example/main.cpp
@@ -5,39 +5,6 @@ int main(int argc, char *argv[])
 {
     Recurse app(argc, argv);
 
-    qDebug() << "start listening...";
-
-    // default server setup
-    // app.listen(3000);
-
-    // manual http server setup *OLD BEHAVIOUR* - info will be removed
-    /*
-    HttpServer http;
-
-    bool bound = http.compose(3000);
-
-    if (!bound) {
-        qDebug() << "could not bind to port 3000";
-        return 0;
-    }
-
-    app.listen(&http);
-    */
-
-    // manual https server setup
-    /*
-    HttpsServer https;
-
-    QHash<QString, QVariant> options;
-    options["port"] = 3000;
-    options["private_key"] = "./priv.pem";
-    options["certificate"] = "./cert.pem";
-
-    if (!https.compose(options)) {
-        exit(1);
-    }
-    */
-
     // http options
     QHash<QString, QVariant> http_options;
     http_options["port"] = 3000;
@@ -50,6 +17,8 @@ int main(int argc, char *argv[])
 
     app.http_server(http_options);
     app.https_server(https_options);
+
+    qDebug() << "start listening...";
 
     auto ret = app.listen();
     if (ret.error()) {
